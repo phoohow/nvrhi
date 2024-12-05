5ef430e9c789183e0bb387ae114e8ea148a441cf
phoohow <phoohow@126.com>
2024-12-05 10:16:38 +0800

    config buildir

--- xmake.lua
+++ xmake.lua
@@ -3,6 +3,9 @@ add_rules("mode.debug", "mode.release")
 
 target("nvrhi")
     set_kind("shared")
+    if get_config("buildir")~=nil then 
+        set_targetdir(get_config("buildir").."/bin")
+    end
     add_includedirs("include", "thirdparty/Vulkan-Headers/include", {
         public = true
     })
