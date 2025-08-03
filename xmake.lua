da10b459721326d29cfd4b922987abc40f8df086
phoohow <phoohow@126.com>
2025-08-03 10:14:45 +0800

    update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,17 +1,16 @@
 set_languages("c++20")
 add_rules("mode.debug", "mode.release")
 
-target("nvrhi")
+target("nvrhi", function()
     set_kind("shared")
-    if get_config("buildir")~=nil then 
-        set_targetdir(get_config("buildir").."/bin")
-    end
-    add_includedirs("include", "thirdparty/Vulkan-Headers/include", {
-        public = true
-    })
+    add_includedirs("include", "thirdparty/Vulkan-Headers/include", {public = true})
     add_files("src/common/*.cpp", "src/validation/*.cpp", "src/d3d12/*.cpp", "src/vulkan/*.cpp")
     add_defines("VULKAN_HPP_STORAGE_SHARED", "VULKAN_HPP_STORAGE_SHARED_EXPORT", "VK_USE_PLATFORM_WIN32_KHR", "NOMINMAX")
     add_defines("NVRHI_SHARED_LIBRARY_BUILD")
-    add_links("dxgi", "d3d12")
+    
+    if is_plat("windows") then
+        add_links("dxgi", "d3d12")
+    end
+    
     add_headerfiles("include/nvrhi/*.h", "include/nvrhi/common/*.h")
-target_end()
+end)
